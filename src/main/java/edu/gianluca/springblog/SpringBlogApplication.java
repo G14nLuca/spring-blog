2843aa4165095d7f8a44ac096de71ccc8595cc8b
Gian Luca Oliveira <gloliveira539@gmail.com>
2022-04-16 18:10:09 -0300

    Initial setup

---
+++ src/main/java/edu/gianluca/springblog/SpringBlogApplication.java
@@ -0,0 +1,13 @@
+package edu.gianluca.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogApplication.class, args);
+	}
+
+}
